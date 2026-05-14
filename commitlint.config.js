3ae7ed9fc984791c8bab70f9df6c33dbcb0719d0
van de Velde, Einar <einar.van.de.velde@helse-vest-ikt.no>
2026-05-14 13:53:59 +0200

    chore(config): enhance commitlint rules with stricter validations and plugins
    Signed-off-by: van de Velde, Einar <einar.van.de.velde@helse-vest-ikt.no>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,33 +1,56 @@
 ﻿export default {
   extends: ["@commitlint/config-conventional"],
+  plugins: [
+    {
+      rules: {
+        "no-duplicate-breaking": ({ header, body, footer }) => {
+          const hasExclamation = /^[a-z]+(\(.+\))?!:/.test(header);
+          const bodyHasBreaking = /BREAKING[ -]CHANGE/i.test(body || "");
+          const footerHasBreaking = /BREAKING[ -]CHANGE/i.test(footer || "");
+          const pass = !(
+            hasExclamation &&
+            (bodyHasBreaking || footerHasBreaking)
+          );
+          return [
+            pass,
+            "Use either `!:` in the header or `BREAKING CHANGE` in the body/footer, not both. Using both causes duplicate entries in the changelog.",
+          ];
+        },
+      },
+    },
+  ],
   rules: {
-    "type-enum": [
-      2,
-      "always",
-      [
-        "feat",
-        "fix",
-        "docs",
-        "style",
-        "refactor",
-        "perf",
-        "test",
-        "build",
-        "ci",
-        "chore",
-        "revert",
-      ],
-    ],
-    "type-case": [2, "always", "lower-case"],
-    "type-empty": [2, "never"],
-    "subject-empty": [2, "never"],
+    "body-leading-blank": [1, "always"],
+    "body-max-line-length": [2, "always", 100],
+    "footer-leading-blank": [1, "always"],
+    "footer-max-line-length": [2, "always", 100],
+    "header-max-length": [2, "always", 100],
+    "no-duplicate-breaking": [2, "always"],
     "subject-case": [
       2,
       "never",
       ["sentence-case", "start-case", "pascal-case", "upper-case"],
     ],
-    "header-max-length": [2, "always", 100],
-    "body-max-line-length": [2, "always", 100],
-    "footer-max-line-length": [2, "always", 100],
+    "subject-empty": [2, "never"],
+    "subject-full-stop": [2, "never", "."],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
   },
 };
